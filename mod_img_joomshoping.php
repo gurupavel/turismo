baed13ef6862ad2569cfd13620831b4477c7d78a
Paul A. Koba <gurupavelu@bigmir.net>
2016-02-21 19:52:22 +0200

    new file:   en-GB.mod_img_joomshoping.ini 	new file:   en-GB.mod_img_joomshoping.sys.ini 	new file:   index.html 	new file:   mod_img_joomshoping.php 	new file:   mod_img_joomshoping.xml 	new file:   tmpl/default.php 	new file:   tmpl/index.html 	new file:   tmpl/success.php

---
+++ mod_img_joomshoping.php
@@ -0,0 +1,29 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+
+if(isset($_POST['mod_img_joomshoping_submitted']))
+{
+	$name = isset($_POST['feedback_name']) ? $_POST['feedback_name'] : 'Не указано' ;
+	$phone = isset($_POST['feedback_phone']) ? $_POST['feedback_phone'] : 'Не указано' ;
+	$adress = isset($_POST['feedback_adress']) ? $_POST['feedback_adress'] : 'Не указано' ;
+	
+	$name = strip_tags($name); 
+	$phone = strip_tags($phone); 
+	$adress = strip_tags($adress); 
+	
+	$message = "<h2>Сообщение с сайта: $params[title]</h2>" ;
+	$message .= "<p>Имя: $name" ;
+	$message .= "<p>Телефон: $phone" ;
+	$message .= "<p>Адрес: $adress" ;
+	
+	$headers= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
+	echo $message;
+	@mail($params['email'], $params['title'], $message, $headers) ;
+
+    require( JModuleHelper::getLayoutPath( 'mod_img_joomshoping', "success" ) );
+}
+else
+{
+    require( JModuleHelper::getLayoutPath( 'mod_img_joomshoping' ) );
+}
