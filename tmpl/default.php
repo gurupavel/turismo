5f5908eeb56411ebdb83cb2d0dab8f3183021bbb
Paul A. Koba <gurupavelu@bigmir.net>
2016-02-22 14:42:13 +0200

    modified:   mod_img_joomshoping.xml 	modified:   tmpl/default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -7,6 +7,15 @@
         <input type="text" name="feedback_adress" class="input-block-level" placeholder="Адрес (улица, дом)" required='required' />
         
         <input type="submit" value="Подключиться" name="mod_img_joomshoping_submitted" class="btn btn-large btn-block btn-info" />
-    
+
+    </form>
+</div>
+
+
+<div id="test_form">
+    <form method="POST">
+        <legend><?=$params['title']?></legend>
+        <input type="submit" value="DB request" name="mod_img_joomshoping_db_request" class="btn btn-large btn-block btn-info" />
+
     </form>
 </div>
